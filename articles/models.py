89c814550ed8b03fddf428d041113533c92d3d9f
Sean <iamseanhong@gmail.com>
2019-11-27 12:25:55 -0800

    Use interactive shell to save article

--- articles/models.py
+++ articles/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField(max_len=100)
+    title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
