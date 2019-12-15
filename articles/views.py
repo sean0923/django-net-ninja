0d6fc12f03b1dda3e5511601c371235796882d4c
Sean <iamseanhong@gmail.com>
2019-12-14 18:27:23 -0800

    Implement 14,15 url parmas and named_url

--- articles/views.py
+++ articles/views.py
@@ -9,3 +9,7 @@ def article_list(request):
     # return HttpResponse('articles/articles')
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
--- django_ninja/urls.py
+++ django_ninja/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -24,3 +25,5 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('', views.homepage),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
